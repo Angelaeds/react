3f8f98cce1fb83a7c6aad62965bb6c042fd660a7
Angelaeds <120844787+Angelaeds@users.noreply.github.com>
2023-10-18 18:25:09 -0300

    Add files via upload

---
+++ componentes/Banner/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Banner from './imagens/banner.png';
+
+
+
+function Banner() {
+    return (
+        <img src={Banner}/> 
+    )
+}
+
+export default Banner
+
+
+
